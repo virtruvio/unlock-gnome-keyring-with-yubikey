fa8bc9d1890e82ae1978e74ed2583a195f402735
iablon <64681499+iablon@users.noreply.github.com>
2023-05-06 09:24:18 +0200

    Update setup-unlocker.sh

--- setup-unlocker.sh
+++ setup-unlocker.sh
@@ -2,8 +2,11 @@
 echo "Starting initilìalisation..."
 echo "moving the udev rule..."
 mv .80-insert-yubi.rules /etc/udev/rules.d/
+echo "making scripts executable"
+chmod +x unlocker.sh
+chmod +x trigger.sh
 echo "moving the scripts.."
-mv .unlocker.sh /usr/local/bin
-mv .trigger.sh /usr/local/bin
+mv unlocker.sh /usr/local/bin
+mv trigger.sh /usr/local/bin
 echo "cleaning..."
 echo "done!"
