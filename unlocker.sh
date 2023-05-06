d8f033c8bfc16d2a1d68a06db242684a4e624f92
iablon <64681499+iablon@users.noreply.github.com>
2023-05-06 09:26:17 +0200

    Update and rename u.sh to unlocker.sh

--- unlocker.sh
+++ unlocker.sh
@@ -2,9 +2,9 @@
 if [[ "$1" == "unlock" ]]; then
 	echo -n $2 | gnome-keyring-daemon -r --unlock
 	if [[ $? -eq 0 ]];then
-		echo "keyring unlocked">>/home/hak/projects/scripts/arg
+		echo "keyring unlocked">>~/keyring-unlocker.log
 		notify-send --hint=int:transient:1 "Keyring"  "Unlocked"
 	else
-		echo "failed to unlock keyring">>/home/hak/projects/scripts/arg
+		echo "failed to unlock keyring">>~/keyring-unlocker.log
 	fi
 fi
